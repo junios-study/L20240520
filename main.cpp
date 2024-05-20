bbdfa4cce1029572764489396be0173ac0f0f9f0
junios <junios@naver.com>
2024-05-20 16:05:29 +0900

    STL 맛보기

--- main.cpp
+++ main.cpp
@@ -1,38 +1,44 @@
 #include <iostream>
+#include <vector>
+#include <list>
 
 using namespace std;
 
-void InsertAfter(int* Numbers, int Size, int Value)
-{
-	//제일 뒤에 자료 추가
-	int* Temp = new int[Size + 1];
 
-	for (int i = 0; i < Size; ++i)
-	{
-		Temp[i] = Numbers[i];
-	}
-	Temp[Size] = Value;
+//STL
 
-	delete[] Numbers;
-	Numbers = Temp;
-
-	for (int i = 0; i < Size + 1; ++i)
-	{
-		cout << Numbers[i] << ",";
-	}
-}
 
 int main()
 {
-	//[][][][][] //자료 구조
-	int Size = 10;
-	int* Numbers = new int[Size];
-	for (int i = 0; i < Size; ++i)
+	vector<int> IntArray;
+
+	IntArray.push_back(1);
+	IntArray.push_back(5);
+	IntArray.push_back(3);
+	IntArray.push_back(2);
+	IntArray.push_back(4);
+
+
+	cout << IntArray[0] << endl;
+	cout << IntArray[1] << endl;
+	cout << IntArray[2] << endl;
+	cout << IntArray[3] << endl;
+	cout << IntArray[4] << endl;
+
+	list<int> IntList;
+	IntList.push_back(1);
+	IntList.push_back(5);
+	IntList.push_back(3);
+	IntList.push_back(2);
+	IntList.push_back(4);
+	IntList.insert(find(IntList.begin(), IntList.end(), 2), 10);
+
+
+	for (auto& Data : IntList)
 	{
-		Numbers[i] = i + 1;
+		cout << Data << endl;
 	}
 
-	InsertAfter(Numbers, Size, 20);
 
 
 
