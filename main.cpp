648e50dd3f30087752cd06855dfc9bcfa802b088
junios <junios@naver.com>
2024-05-20 13:59:29 +0900

    포인터 예제

--- main.cpp
+++ main.cpp
@@ -1,41 +1,22 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
+int A;
+
 int main()
 {
-	//C++, BP
-	//int, float , bool, char, string
-	string Message = "안녕하세요.";
-	string Message2 = "세상아";
 
-	cout << Message + Message2 << endl;
+	int A = 0;
+	int* P = new int;
 
-	//char H[5] = { 'H', 'E', 'L', 'L', 'O' };
+	*P = 10;
 
-	//for (int i = 0; i < 5; ++i)
-	//{
-	//	cout << H[i];
-	//}
-	//cout << endl;
+	cout << *P << endl;
 
-	//char M[6] = { 'H', 'E', 'L', 'L', 'O', '\0'};
-	char M[6] = { "HE\nLO" }; 
-
-	cout << M << endl;
-
-	//for (int i = 0; i < 6; ++i)
-	//{
-	//	if (M[i] == 0)
-	//	{
-	//		cout << i << endl;
-	//		break;
-	//	}
-	//}
-	//cout << strlen(M) << endl;
 
 	
+	delete P;
 
 	return 0;
 }
